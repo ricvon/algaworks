34b18b30a7568dde0e2cefdabafc3a515979811c
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-07-06 01:07:12 -0400

    altafood 2.5.0

---
+++ src/main/java/br/com/rv/algafood/MeuPrimeiroController.java
@@ -0,0 +1,14 @@
+package br.com.rv.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MeuPrimeiroController {
+		@GetMapping("/hello")
+		@ResponseBody
+		public String hello() {
+			return "hello";
+		}
+}

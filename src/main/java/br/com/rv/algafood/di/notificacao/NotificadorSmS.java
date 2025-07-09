ab66afe4487e3902802db675e4f56ac3ab588a40
Ricardo espirito santo <ricardo.santo@gmail.com>
2025-07-09 10:26:06 -0400

    comitando master com versão 2.15

---
+++ src/main/java/br/com/rv/algafood/di/notificacao/NotificadorSmS.java
@@ -0,0 +1,26 @@
+package br.com.rv.algafood.di.notificacao;
+
+import br.com.rv.algafood.di.modelo.Cliente;
+
+public class NotificadorSmS implements Notificador{
+	private boolean caixaAuta;
+	private String hostServidorSmtp;
+	
+	public NotificadorSmS(String hostServidorSmtp) {
+		this.hostServidorSmtp=hostServidorSmtp;
+		System.out.println("Construtor Notificação de Email Chamado");
+	}
+
+	@Override
+	public void notificar (Cliente cliente, String mensagem) {
+		if (this.caixaAuta) {
+			mensagem = mensagem.toUpperCase();
+		}
+		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
+	}
+
+	public void setCaixaAuta(boolean caixaAuta) {
+		this.caixaAuta = caixaAuta;
+	}
+
+}
